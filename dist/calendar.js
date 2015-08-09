304532da14fae92c2ebe5e79a5eaee9e43e6fbfd
Mujib517 <mujib517@gmail.com>
2015-08-09 12:41:50 +0530

    Bug fixes and grunt release

--- dist/calendar.js
+++ dist/calendar.js
@@ -258,6 +258,7 @@ app.directive('myCalendar', function () {
     "    .btn-width {\n" +
     "        width: 80px;\n" +
     "    }\n" +
+    "\n" +
     "    .input-xs {\n" +
     "        height: 22px;\n" +
     "        padding: 2px 5px;\n" +
@@ -267,9 +268,6 @@ app.directive('myCalendar', function () {
     "    }\n" +
     "    #tblMnth{\n" +
     "        table-layout: fixed;\n" +
-    "    }\n" +
-    "    #tblMnth tr{\n" +
-    "        white-space: nowrap;\n" +
     "    }\n" +
     "</style>\n" +
     "<div ng-show=\"!yearMode\">\n" +
